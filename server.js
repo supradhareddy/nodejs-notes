b9998d14979cb248947e0eb1f0bb5c646606a6ae
Supradha <supradhareddy.r@gmail.com>
2016-10-27 13:18:32 +0530

    adding server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+var server = http.createServer(function(req,res){
+
+	res.writeHead(200, {"content-Type":"text/plain"});
+	res.end("Hello world!!");
+});
+server.listen(8000, function(){
+	console.log("server listening at port 8000")
+})
